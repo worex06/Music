d3ac3ce758d80f2f94ff31eeb4d4685ad7cb548f
zerosugar6464 <musickumal@gmail.com>
2025-04-17 23:06:13 +0300

    Update weather.py

--- AlexaMusic/plugins/tools/weather.py
+++ AlexaMusic/plugins/tools/weather.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from AlexaMusic import app
 
-timeout = httpx.Timeout(40)
+timeout = httpx.Timeout(1000)
 http = httpx.AsyncClient(http2=True, timeout=timeout)
 
 # Api key used in weather.com's mobile app.
