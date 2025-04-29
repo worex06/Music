914696c2aafd244a8a0efacbf84acbc306b11d4b
zerosugar6464 <musickumal@gmail.com>
2025-04-29 20:57:56 +0300

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
 
 import sys
 
@@ -6,23 +17,41 @@ import config
 from ..logging import LOGGER
 from pyrogram.enums import ChatMemberStatus
 
-class AlexaBot: def init(self): self.name = "Alexa" self.running = False
 
-async def start(self):
-    self.running = True
-    LOGGER(__name__).info(f"{self.name} botu başlatıldı.")
-    try:
-        while self.running:
-            LOGGER(__name__).info(f"{self.name} çalışıyor...")
-            await asyncio.sleep(10)
-    except asyncio.CancelledError:
-        LOGGER(__name__).info("Bot durduruldu.")
+class AlexaBot(Client):
+    def __init__(self):
+        super().__init__(
+            "MusicBot",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+            sleep_threshold=180,
+            max_concurrent_transmissions=4,
+            workers=50,
+        )
+        LOGGER(__name__).info(f"Starting Bot...")
 
-async def stop(self):
-    self.running = False
-    LOGGER(__name__).info(f"{self.name} botu durduruldu.")
-
-Eğer doğrudan çalıştırılırsa
-
-if name == "main": bot = AlexaBot() loop = asyncio.get_event_loop() try: loop.run_until_complete(bot.start()) except KeyboardInterrupt: loop.run_until_complete(bot.stop())
-
+    async def start(self):
+        await super().start()
+        get_me = await self.get_me()
+        self.username = get_me.username
+        self.id = get_me.id
+        self.mention = get_me.mention
+        try:
+            await self.send_message(
+                config.LOG_GROUP_ID, "» Mehmet kullanılmaya mahkümdur..."
+            )
+        except:
+            LOGGER(__name__).error(
+                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+            )
+            sys.exit()
+        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+        if a.status != ChatMemberStatus.ADMINISTRATOR:
+            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
+            sys.exit()
+        if get_me.last_name:
+            self.name = get_me.first_name + " " + get_me.last_name
+        else:
+            self.name = get_me.first_name
+        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
