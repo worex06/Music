2c19529148d7a412bf2c38a22c3f5d99db986192
zerosugar6464 <musickumal@gmail.com>
2025-04-29 19:36:06 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -48,7 +48,7 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/zerosugar6464/Kumsal
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
-GIT_TOKEN = getenv("GIT_TOKEN", None)
+GIT_TOKEN = getenv("GIT_TOKEN", "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")
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/the_team_kumsal")
 
