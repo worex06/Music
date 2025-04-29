304c98e1654ddbe9b00bb5bace193ff37664739a
zerosugar6464 <musickumal@gmail.com>
2025-04-29 21:26:36 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -29,7 +29,8 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "900"))
 
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
 
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+LOG = True  # veya False
+LOG_GROUP_ID = -1001234567890  # Telegram grup ID'si
 
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Kumsal Music bot")
 
