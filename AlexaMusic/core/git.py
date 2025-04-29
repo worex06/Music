ee6f3a3b1b25b250ec4c39cd6e363f2995a9ff6c
zerosugar6464 <musickumal@gmail.com>
2025-04-29 21:16:30 +0300

    Update git.py

--- AlexaMusic/core/git.py
+++ AlexaMusic/core/git.py
@@ -1,5 +1,15 @@
-import os
-import shutil
+# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
 import asyncio
 import shlex
 from typing import Tuple
@@ -8,9 +18,9 @@ from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
 
 import config
+
 from ..logging import LOGGER
 
-loop = asyncio.get_event_loop_policy().get_event_loop()
 
 def install_req(cmd: str) -> Tuple[str, str, int, int]:
     async def install_requirements():
@@ -28,13 +38,10 @@ def install_req(cmd: str) -> Tuple[str, str, int, int]:
             process.pid,
         )
 
-    return loop.run_until_complete(install_requirements())
+    return asyncio.get_event_loop().run_until_complete(install_requirements())
+
 
 def git():
-    if not shutil.which("git"):
-        LOGGER(__name__).info("Git komutu bulunamadı, güncelleme işlemi atlanıyor.")
-        return
-
     REPO_LINK = config.UPSTREAM_REPO
     if config.GIT_TOKEN:
         GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
@@ -42,53 +49,35 @@ def git():
         UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
     else:
         UPSTREAM_REPO = config.UPSTREAM_REPO
-
     try:
-        repo = Repo(os.getcwd())
+        repo = Repo()
         LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
-    except (GitCommandError, InvalidGitRepositoryError):
+    except GitCommandError:
+        LOGGER(__name__).info(f"Invalid Git Command")
+    except InvalidGitRepositoryError:
+        repo = Repo.init()
+        if "origin" in repo.remotes:
+            origin = repo.remote("origin")
+        else:
+            origin = repo.create_remote("origin", UPSTREAM_REPO)
+        origin.fetch()
+        repo.create_head(
+            config.UPSTREAM_BRANCH,
+            origin.refs[config.UPSTREAM_BRANCH],
+        )
+        repo.heads[config.UPSTREAM_BRANCH].set_tracking_branch(
+            origin.refs[config.UPSTREAM_BRANCH]
+        )
+        repo.heads[config.UPSTREAM_BRANCH].checkout(True)
         try:
-            repo = Repo.init(os.getcwd())
-            if "origin" in repo.remotes:
-                origin = repo.remote("origin")
-            else:
-                origin = repo.create_remote("origin", UPSTREAM_REPO)
-            origin.fetch()
-            repo.create_head(
-                config.UPSTREAM_BRANCH,
-                origin.refs[config.UPSTREAM_BRANCH],
-            )
-            repo.heads[config.UPSTREAM_BRANCH].set_tracking_branch(
-                origin.refs[config.UPSTREAM_BRANCH]
-            )
-            repo.heads[config.UPSTREAM_BRANCH].checkout(True)
-        except Exception as e:
-            LOGGER(__name__).warning(f"Git işlemi başarısız: {e}")
-            return
-
-    try:
+            repo.create_remote("origin", config.UPSTREAM_REPO)
+        except BaseException:
+            pass
         nrs = repo.remote("origin")
         nrs.fetch(config.UPSTREAM_BRANCH)
-    except Exception as e:
-        LOGGER(__name__).warning(f"Uzak repo fetch başarısız: {e}")
-        return
-
-    requirements_file = "requirements.txt"
-    try:
-        diff_index = repo.head.commit.diff("FETCH_HEAD")
-        requirements_updated = any(
-            diff.a_path == requirements_file or diff.b_path == requirements_file
-            for diff in diff_index
-        )
-    except Exception:
-        requirements_updated = False
-
-    try:
-        nrs.pull(config.UPSTREAM_BRANCH)
-    except GitCommandError:
-        repo.git.reset("--hard", "FETCH_HEAD")
-
-    if requirements_updated:
+        try:
+            nrs.pull(config.UPSTREAM_BRANCH)
+        except GitCommandError:
+            repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -r requirements.txt")
-
         LOGGER(__name__).info(f"Fetched Updates from: {REPO_LINK}")
