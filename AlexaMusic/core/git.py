ace8c799277bcc38c86bea9a823c1f5ae8c474e6
zerosugar6464 <musickumal@gmail.com>
2025-04-29 20:02:43 +0300

    Update git.py

--- AlexaMusic/core/git.py
+++ AlexaMusic/core/git.py
@@ -1,13 +1,5 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
+import os
+import shutil
 import asyncio
 import shlex
 from typing import Tuple
@@ -16,11 +8,9 @@ from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
 
 import config
-
 from ..logging import LOGGER
 
 loop = asyncio.get_event_loop_policy().get_event_loop()
-
 
 def install_req(cmd: str) -> Tuple[str, str, int, int]:
     async def install_requirements():
@@ -40,8 +30,11 @@ def install_req(cmd: str) -> Tuple[str, str, int, int]:
 
     return loop.run_until_complete(install_requirements())
 
-
 def git():
+    if not shutil.which("git"):
+        LOGGER(__name__).info("Git komutu bulunamadı, güncelleme işlemi atlanıyor.")
+        return
+
     REPO_LINK = config.UPSTREAM_REPO
     if config.GIT_TOKEN:
         GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
@@ -51,41 +44,44 @@ def git():
         UPSTREAM_REPO = config.UPSTREAM_REPO
 
     try:
-        repo = Repo()
+        repo = Repo(os.getcwd())
         LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
-    except GitCommandError:
-        LOGGER(__name__).info(f"Invalid Git Command")
-    except InvalidGitRepositoryError:
-        repo = Repo.init()
-        if "origin" in repo.remotes:
-            origin = repo.remote("origin")
-        else:
-            origin = repo.create_remote("origin", UPSTREAM_REPO)
-        origin.fetch()
-        repo.create_head(
-            config.UPSTREAM_BRANCH,
-            origin.refs[config.UPSTREAM_BRANCH],
-        )
-        repo.heads[config.UPSTREAM_BRANCH].set_tracking_branch(
-            origin.refs[config.UPSTREAM_BRANCH]
-        )
-        repo.heads[config.UPSTREAM_BRANCH].checkout(True)
-
+    except (GitCommandError, InvalidGitRepositoryError):
         try:
-            repo.create_remote("origin", config.UPSTREAM_REPO)
-        except Exception:
-            pass
+            repo = Repo.init(os.getcwd())
+            if "origin" in repo.remotes:
+                origin = repo.remote("origin")
+            else:
+                origin = repo.create_remote("origin", UPSTREAM_REPO)
+            origin.fetch()
+            repo.create_head(
+                config.UPSTREAM_BRANCH,
+                origin.refs[config.UPSTREAM_BRANCH],
+            )
+            repo.heads[config.UPSTREAM_BRANCH].set_tracking_branch(
+                origin.refs[config.UPSTREAM_BRANCH]
+            )
+            repo.heads[config.UPSTREAM_BRANCH].checkout(True)
+        except Exception as e:
+            LOGGER(__name__).warning(f"Git işlemi başarısız: {e}")
+            return
 
-    nrs = repo.remote("origin")
-    nrs.fetch(config.UPSTREAM_BRANCH)
+    try:
+        nrs = repo.remote("origin")
+        nrs.fetch(config.UPSTREAM_BRANCH)
+    except Exception as e:
+        LOGGER(__name__).warning(f"Uzak repo fetch başarısız: {e}")
+        return
 
     requirements_file = "requirements.txt"
-    diff_index = repo.head.commit.diff("FETCH_HEAD")
-
-    requirements_updated = any(
-        diff.a_path == requirements_file or diff.b_path == requirements_file
-        for diff in diff_index
-    )
+    try:
+        diff_index = repo.head.commit.diff("FETCH_HEAD")
+        requirements_updated = any(
+            diff.a_path == requirements_file or diff.b_path == requirements_file
+            for diff in diff_index
+        )
+    except Exception:
+        requirements_updated = False
 
     try:
         nrs.pull(config.UPSTREAM_BRANCH)
@@ -95,4 +91,4 @@ def git():
     if requirements_updated:
         install_req("pip3 install --no-cache-dir -r requirements.txt")
 
-    LOGGER(__name__).info(f"Fetched Updates from: {REPO_LINK}")
+    LOGGER(__name__).info(f"Güncellemeler çekildi: {REPO_LINK}")
