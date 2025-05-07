3157a93213b4523939b4b2ebe9f608f023a244fc
ramowlf <ramodiscord26@gmail.com>
2025-05-07 15:15:40 +0300

    Update help.py

--- AlexaMusic/utils/inline/help.py
+++ AlexaMusic/utils/inline/help.py
@@ -42,10 +42,6 @@ def help_pannel(_, START: Union[bool, int] = None):
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
                     text=_["H_B_7"],
                     callback_data="help_callback hb7",
                 ),
